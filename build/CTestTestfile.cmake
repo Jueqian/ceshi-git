acd824c89e88e20fd9969b9184dd5d86ca252d26
Jueqian <halo.qdx@qq.com>
2024-03-14 21:56:46 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xxd/workspace/projects/a-ceshi
+# Build directory: /home/xxd/workspace/projects/a-ceshi/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from demo!\n" << std::endl;
+
+    std::cout << "Hello1, from demo!\n" << std::endl;
+
+    std::cout << "Hello2, from demo!\n" << std::endl;
+
+    std::cout << "Hello3, from demo!\n" << std::endl;
+
+    std::cout << "Hello4, from demo!\n" << std::endl;
+
+    std::cout << "Hello5, from demo!\n" << std::endl;
+}
